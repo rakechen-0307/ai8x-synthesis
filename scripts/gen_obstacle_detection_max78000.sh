12bcc0c6124e9db3abb4a3519a1dfb61184f0042
rakechen-0307 <rakechen168@gmail.com>
2024-07-11 17:37:28 +0800

    change synthesis

--- scripts/gen_obstacle_detection_max78000.sh
+++ scripts/gen_obstacle_detection_max78000.sh
@@ -1,6 +1,1 @@
-#!/bin/sh
-DEVICE="MAX78000"
-TARGET="sdk/Examples/$DEVICE/CNN"
-COMMON_ARGS="--device $DEVICE --timer 0 --display-checkpoint --verbose"
-
-python ai8xize.py --test-dir $TARGET --prefix obstacle_detection --checkpoint-file trained/obstacle_detection-qat8-q.pth.tar --config-file networks/obstacle_detection.yaml --fifo  $COMMON_ARGS "$@"
+python ai8xize.py --test-dir sdk/Examples/MAX78000/CNN --prefix obstacle_detection --checkpoint-file trained/obstacle_detection-qat8-q.pth.tar --config-file networks/obstacle_detection.yaml --overlap-data --device MAX78000 --timer 0 --display-checkpoint --verbose
