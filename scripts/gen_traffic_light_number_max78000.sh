358e3102a58874ebc79553d1b16729f9306857b3
rakechen-0307 <rakechen168@gmail.com>
2024-07-12 09:35:28 +0800

    change synthesis

--- scripts/gen_traffic_light_number_max78000.sh
+++ scripts/gen_traffic_light_number_max78000.sh
@@ -1,6 +1,1 @@
-#!/bin/sh
-DEVICE="MAX78000"
-TARGET="sdk/Examples/$DEVICE/CNN"
-COMMON_ARGS="--device $DEVICE --timer 0 --display-checkpoint --verbose"
-
-python ai8xize.py --test-dir $TARGET --prefix traffic_light_number --checkpoint-file trained/traffic_light_number-qat8-q.pth.tar --config-file networks/traffic_light_number.yaml --fifo  $COMMON_ARGS "$@"
+python ai8xize.py --test-dir sdk/Examples/MAX78000/CNN --prefix traffic_light_number --checkpoint-file trained/traffic_light_number-qat8-q.pth.tar --config-file networks/traffic_light_number.yaml --overlap-data --device MAX78000 --timer 0 --display-checkpoint --verbose
